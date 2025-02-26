037a6340fa8fc8bea827faef8cd3656f401cb7d5
Martin Flory <martin@rotundasoftware.com>
2025-02-26 10:17:16 -0300

    fix typo

--- index.mjs
+++ index.mjs
@@ -1,4 +1,4 @@
-export { default as CollectionService } from './services/appCollectionService.mjs';
+export { default as CollectionService } from './services/collectionService.mjs';
 export { default as Baseline } from './baseline.mjs';
 export { default as BaseStrategy } from './baseStrategy.mjs';
 export { default as BaseService } from './services/baseService.mjs';
